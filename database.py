9fb60431322a298709776d796f7fe2026f212c04
vishnivetskiikirill-art <vishnivetskiikirill@gmail.com>
2025-12-16 21:49:30 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,15 +1,20 @@
+import os
 import asyncpg
-from config import DATABASE_URL
 
 pool = None
 
 async def connect_db():
     global pool
-    pool = await asyncpg.create_pool(DATABASE_URL)
+    database_url = os.getenv("DATABASE_URL")
+    if not database_url:
+        raise RuntimeError("DATABASE_URL is not set")
+    pool = await asyncpg.create_pool(database_url)
+
 
 async def execute(query, *args):
     async with pool.acquire() as conn:
         return await conn.execute(query, *args)
+
 
 async def fetch(query, *args):
     async with pool.acquire() as conn:
