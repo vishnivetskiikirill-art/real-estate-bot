add0650904bc17efc6598ce47b61bce51b3237d5
vishnivetskiikirill-art <vishnivetskiikirill@gmail.com>
2026-01-14 12:34:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
-DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
+if not TELEGRAM_TOKEN:
+    raise RuntimeError("TELEGRAM_TOKEN is not set")
 
-if not BOT_TOKEN:
-    raise RuntimeError("BOT_TOKEN is not set")
 if not DATABASE_URL:
     raise RuntimeError("DATABASE_URL is not set")
