2395cf4565b3c10ce1aba6cde95d10d7918af307
vishnivetskiikirill-art <vishnivetskiikirill@gmail.com>
2026-01-15 12:00:04 +0200

    Update __init__.py

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,23 @@
-from .db import init_db, close_db, fetch_properties
+from .db import (
+    init_db,
+    close_db,
+    fetch_properties,
+    get_user_lang,
+    set_user_lang,
+    set_user_city,
+    set_user_district,
+    set_user_property_type,
+    get_user_filters,
+)
 
-all = ["init_db", "close_db", "fetch_properties"]
+__all__ = [
+    "init_db",
+    "close_db",
+    "fetch_properties",
+    "get_user_lang",
+    "set_user_lang",
+    "set_user_city",
+    "set_user_district",
+    "set_user_property_type",
+    "get_user_filters",
+]
